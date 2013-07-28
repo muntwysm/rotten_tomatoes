2ce1cbe6f4c3d182ceb1526493bd38c3496d74cd
muntwysm <stefan.muntwyler@gmail.com>
2013-07-28 11:28:53 +0200

    Adds movie filter

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,12 @@ class Movie < ActiveRecord::Base
 	#default_scope order('title ASC')
 
 	def self.get_ratings
-		Movie.all(:select => "distinct(rating)").collect { |m| m.rating }
+		ratings = []
+		collection = Movie.all(:select => "DISTINCT(rating)")
+		collection.each do |movie|
+			ratings << movie.rating
+		end
+		return ratings
+		#Movie.all(:select => "distinct(rating)").collect { |m| m.rating }
 	end
 end
